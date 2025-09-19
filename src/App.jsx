aeb031243fd04803a306ca6b571dcfe0bbeab3f0
Codexa <codexa.build@gmail.com>
2025-09-19 13:54:27 -0400

    Fix Vite build configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import { AppProvider } from '@shopify/polaris';
 import SettingsPage from './components/SettingsPage';
-import '@shopify/polaris/dist/styles.css';
+import '@shopify/polaris/build/esm/styles.css';
 
 function App() {
   return (
